cadfa8b98a2d1320ba11d66b9753024c3600b6fc
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-24 10:15:36 +0300

    added some bacekedn code

--- server/controllers/authcontroler.js
+++ server/controllers/authcontroler.js
@@ -1,20 +1,71 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/usersmodel";
 
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "missing details" });
+  }
+  try {
+    const existingUser = await userModel.findOne({ email });
 
-export const regiser = async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-        return res.json({success:false, message:'missing details'})
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SCRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      
+    });
+    return res.json({ success: true });
+  } catch {
+    error;
+  }
+  {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({success:false, message:'email and passwrod required'})
     }
     try {
-        const existingUser = await userModel
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.json({success:false, message:'invaild email'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({success:false,message:'invaild passwrod'})
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SCRET, {
+            expiresIn: "7d",
+          });
+      
+          res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+          });
+        return res.json({ success: true });
 
-
-        const hashedPassword = await bcrypt.hash(password, 10);
 
         
-    } catch { error } {
-        res.json({success:false, message: error.message})
+    } catch (error) {
+        return res.json({success:false, message:error.message})
     }
 }
